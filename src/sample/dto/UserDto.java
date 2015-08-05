a750bde05e5f73e2fb565dffd11adbd0925d7f08
DAVID_swingfox95 <drthrax.ramirez@gmail.com>
2015-08-05 22:02:53 +0800

    removed blank constructor in UserDto and put some comments in TaskDto

--- src/sample/dto/UserDto.java
+++ src/sample/dto/UserDto.java
@@ -22,8 +22,6 @@ public class UserDto {
         private String email;
         private List<String> errorList;
         
-        public UserDto(){}
-        
         public List<String> getErrorList() {
             return errorList;
         }
